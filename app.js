ab844c63134bab9bf09f138452e71c6a8124476e
demym <demym@yahoo.it>
2017-09-27 21:33:04 +0200

    Update app.js

--- app.js
+++ app.js
@@ -97,7 +97,8 @@ app.use(cookieParser());
 
 //app.use(app.router());
 
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'clients/client/ion2kwondo/www/')));
 app.use(cors());
 
 
