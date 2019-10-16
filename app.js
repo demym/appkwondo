a38752661ba3c6fe22a98f8d48aa6bc36b285403
Demetrio Mortelliti <demym@yahoo.it>
2019-10-16 11:08:32 +0200

    fdafdafda

--- app.js
+++ app.js
@@ -118,7 +118,29 @@ app.use(cookieParser());
 
 app.use(express.static(path.join(__dirname, 'public')));
 //app.use(express.static(path.join(__dirname, 'www')));
-app.options('*', cors())
+
+
+const allowedOrigins = [
+	'capacitor://localhost',
+	'ionic://localhost',
+	'http://localhost',
+	'http://localhost:8080',
+	'http://localhost:8100'
+  ];
+  
+  // Reflect the origin if it's in the allowed list or not defined (cURL, Postman, etc.)
+  const corsOptions = {
+	origin: (origin, callback) => {
+	  if (allowedOrigins.includes(origin) || !origin) {
+		callback(null, true);
+	  } else {
+		callback(new Error('Origin not allowed by CORS'));
+	  }
+	}
+  }
+  
+  // Enable preflight requests for all routes
+  app.options('*', cors(corsOptions));
 
 
 /*app.use(cors({
@@ -414,7 +436,7 @@ app.post('/login', passport.authenticate('local-signin', {
 //app.use("/installations",installations)
 //app.use("/schede",schede)
 
-app.get('/', function (req, res) {
+app.get('/', cors(corsOptions), function (req, res) {
 
 
 	res.sendfile('index.html', {
